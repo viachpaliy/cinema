ff0b9b4e30cf8896e83c0449390688d8c4d9835a
viachpaliy <https://github.com/viachpaliy/cinema>
2015-11-28 18:53:48 +0200

    class moviehouse add

---
+++ Cinema/MovieHouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    class MovieHouse
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        private Film[] _films;
+        private Auditorium[] _auditoria;
+
+        public void PrintAllFilms()
+        {
+            foreach (var film in _films)
+            {
+                Console.WriteLine(film);
+            }
+        }
+
+
+
+
+
+    }
+}
